716b51b63f3af2e15247839e1df379044a6dcea1
Aissa Desktop <aissagabriella29@gmail.com>
2019-06-14 15:11:57 -0300

    Exercício 2

---
+++ tabela/tabela/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: THGEVER
+ * Date: 14/06/2019
+ * Time: 15:10
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace tabela
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
