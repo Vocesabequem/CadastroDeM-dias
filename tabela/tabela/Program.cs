29d2eda4c8bb39980d4c3f76adb472d4faad485f
Aissa Desktop <aissagabriella29@gmail.com>
2019-06-14 22:35:17 -0300

    Segundo exercício adicionado

--- tabela/tabela/Program.cs
+++ tabela/tabela/Program.cs
@@ -14,12 +14,42 @@ namespace tabela
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string perg1, perg2;
+			bool resp1, resp2;
 			
-			// TODO: Implement Functionality Here
+			Console.WriteLine ("Por favor, digite uma afirmação");
+			perg1 = Console.ReadLine ();
+			Console.WriteLine ("Digite outra afirmação, por favor");
+			perg2 = Console.ReadLine ();
 			
-			Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
+			Console.WriteLine ("A primeira afirmativa é verdadeira ou falsa? Responda com true ou false");
+			resp1 == Console.ReadLine () ;
+		    Console.WriteLine ("A segunda afirmativa é verdadeira ou falsa? Por favor, responda com true ou false");
+			resp2 == Console.ReadLine () ;
+	
+			if (resp1 == true && resp2 == true) {
+				Console.WriteLine("O resultado lógico das afirmativas é: " + resp1 + "e" + resp2 +"é: true");
+			} else {
+				Console.WriteLine("O resultado lógico das afirmativas é: " + resp1 + "ou" + resp2 +"é: true");
+				
+			}
+			if (resp1 == false && resp2 == true) {
+				Console.WriteLine ("O resultado lógico das afirmativas é: " +resp1+ "e" +resp2+ "é: false");
+			} else {
+				Console.WriteLine ("O resultado lógico das afirmativas é: " +resp1+ "ou" +resp2+ "é: true");
+			}
+			if (resp1 == true && resp2 == false) {
+				Console.WriteLine ("O resultado lógico das afirmativas é: " +resp1+ "e" +resp2+ "é: false");
+			} else {
+				Console.WriteLine ("O resultado lógico das afirmativas é: " +resp1+ "ou" +resp2+ "é: true");
+			}
+			if (resp1 == false && resp2 == false) {
+				Console.WriteLine ("O resultado lógico das afirmativas é: " +resp1+ "e" +resp2+ "é: false");
+			} else {
+				Console.WriteLine ("O resultado lógico das afirmativas é: " +resp1+ "ou" +resp2+ "é: false");
+				                 
+			}
+			
 		}
 	}
 }
